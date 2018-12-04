2d2a4d110491bfcea8b5cbbb68cb54f346e30d56
KmolYuan <pyslvs@gmail.com>
2018-12-04 16:04:10 +0800

    line interpolation

--- core/nc.py
+++ core/nc.py
@@ -12,14 +12,51 @@ def nc_code_compiler(path: str):
         nc_doc = f.read().upper()
     
     command = []
-    for m in match(r"G(\d{1,2})\sX([+-]?\d+\.?\d*)\sY([+-]?\d+\.?\d*)(?:\sF(\d+\.?\d*))?", nc_doc):
+    for m in match(
+        r"G(\d{1,2})\s"
+        r"X([+-]?\d+\.?\d*)\s"
+        r"Y([+-]?\d+\.?\d*)"
+        r"(?:\sZ([+-]?\d+\.?\d*))?"
+        r"(?:\sF(\d+\.?\d*))?",
+        nc_doc
+    ):
         g = int(m.group(1))
         x = float(m.group(2))
         y = float(m.group(3))
-        try:
-            f = float(m.group(4))
-        except TypeError:
-            command.append((g, x, y))
+        if m.group(4):
+            z = float(m.group(4))
         else:
-            command.append((g, x, y, f))
-    print(command)
+            z = None
+        if m.group(5):
+            f = float(m.group(5)) / 60.
+        else:
+            f = None
+
+        command.append((g, x, y, z, f))
+
+    return command
+
+
+def plot(
+    v_cmd: float,
+    a_max: float,
+    j_max: float,
+    t0: float,
+    t: float,
+    t1: float
+):
+    """Plot of s, v, a, j."""
+    s = v_cmd * t
+    v = v_cmd
+
+    if t == t0:
+        a = a_max
+        j = j_max
+    elif t == t1:
+        a = -a_max
+        j = -j_max
+    else:
+        a = 0
+        j = 0
+
+    return s, v, a, j
--- launch.py
+++ launch.py
@@ -1,12 +1,27 @@
 # -*- coding: utf-8 -*-
 
+import matplotlib.pyplot as plt
 import yaml
 from numpy import pi
 from core.forward_kinematics import forward_transform
 from core.inverse_kinematics import inverse_transform
-from core.nc import nc_code_compiler
+from core.line_interpolation import line_interpolation_2d
+from core.nc import nc_code_compiler, plot
 from core import print_matrix
 
 
 if __name__ == '__main__':
-    nc_code_compiler("line.nc")
+    command = nc_code_compiler("line.nc")
+    ox = 0
+    oy = 0
+    for g, x, y, z, f in command:
+        if f:
+            v_cmd, a_max, j_max, t_c = line_interpolation_2d(ox, oy, x, y, f)
+            plot_list = []
+            for t in range(100 + 1):
+                s, v, a, j = plot(v_cmd, a_max, j_max, 0, t_c * t / 100, t_c)
+                plot_list.append(a)
+            plt.plot(plot_list)
+            plt.show()
+        ox = x
+        oy = y
